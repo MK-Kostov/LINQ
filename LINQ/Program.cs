e969092849b04d7fad47e4b2058ccf0cbf3bf91f
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-23 09:52:35 -0500

    What is LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,1 +1,63 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LinqTutorial
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var wordsNoUppercase = new string[]
+				{
+					"quick", "brown", "fox"
+				};
+			Console.WriteLine(IsAnyWordUpperCase(wordsNoUppercase));
+			var wordsWithUppercase = new string[]
+				{
+					"quick", "brown", "FOX"
+				};
+
+			Console.WriteLine(IsAnyWordUpperCase(wordsWithUppercase));
+
+			Console.ReadKey();
+		}
+
+		//public static bool IsAnyWordUpperCase(IEnumerable<string> words>)
+		//{
+		//	foreach(var word in words)
+		//	{
+
+		//	}
+		//}
+
+		public static bool IsAnyWordUpperCase_Linq(IEnumerable<string> words)
+		{
+			return words.Any(word => word.All(letter => char.IsUpper(letter)));
+
+		}
+
+			public static bool IsAnyWordUpperCase(IEnumerable<string> words)
+		{
+			foreach (var word in words)
+			{
+				bool areAllUpperCase = true;
+				foreach (var letter in word)
+				{
+					if (char.IsLower(letter))
+					{
+						areAllUpperCase = false;
+					}
+				}
+
+				if (areAllUpperCase)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
