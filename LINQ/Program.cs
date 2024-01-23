df86831bb6e0ccbb0efddf4005b3a06361c7fb9d
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-23 11:31:43 -0500

    LINQ, IEnumerable<T> and method chaining

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -62,12 +62,36 @@
 //	}
 //}
 
-using System.Net;
+//using System.Net;
 
-var words = new List<string> { "a", "bb", "ccc", "dddd" };
-var wordsLongerThan2Letters = words.Where(word => word.Length > 2);
+//var words = new List<string> { "a", "bb", "ccc", "dddd" };
+//var wordsLongerThan2Letters = words.Where(word => word.Length > 2);
 
-var numbers = new int[] { 1, 2, 3, 4, 5, 6 };
-var oddNumbers = numbers.Where(number => number % 2 == 1);
+//var numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+//var oddNumbers = numbers.Where(number => number % 2 == 1);
 
-Console.ReadKey();
+//Console.ReadKey();
+
+namespace LinqTutorial
+{
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var numbers = new List<int> { 5, 3, 7, 1, 2, 4 };
+			var numbersWith10 = numbers.Append(10);
+
+			//Console.WriteLine("Numbers: " + string.Join(", ", numbers));
+			//Console.WriteLine("Numbers with 10: " + string.Join(", ", numbersWith10));
+
+			//var oddNumbers = numbers.Where(number => number % 2 == 1);
+			var orderedOddNumbers = numbers.Where(number => number % 2 == 1)
+				.OrderBy(number => number);
+			Console.WriteLine("orderedOddNumbers: " + string.Join(", ", orderedOddNumbers));
+
+			Console.ReadKey();
+		}
+	}
+}
+
