1fd4f0de8e1626e3f683b74d6d3baff033aa8414
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-23 10:09:37 -0500

    LINQ and extension methods

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,63 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
 
-namespace LinqTutorial
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var wordsNoUppercase = new string[]
-				{
-					"quick", "brown", "fox"
-				};
-			Console.WriteLine(IsAnyWordUpperCase(wordsNoUppercase));
-			var wordsWithUppercase = new string[]
-				{
-					"quick", "brown", "FOX"
-				};
+//namespace LinqTutorial
+//{
+//	class Program
+//	{
+//static void Main(string[] args)
+//{
+//	var wordsNoUppercase = new string[]
+//		{
+//			"quick", "brown", "fox"
+//		};
+//	Console.WriteLine(IsAnyWordUpperCase(wordsNoUppercase));
+//	var wordsWithUppercase = new string[]
+//		{
+//			"quick", "brown", "FOX"
+//		};
 
-			Console.WriteLine(IsAnyWordUpperCase(wordsWithUppercase));
+//	Console.WriteLine(IsAnyWordUpperCase(wordsWithUppercase));
 
-			Console.ReadKey();
-		}
+//	Console.ReadKey();
+//}
 
-		//public static bool IsAnyWordUpperCase(IEnumerable<string> words>)
-		//{
-		//	foreach(var word in words)
-		//	{
+//public static bool IsAnyWordUpperCase(IEnumerable<string> words>)
+//{
+//	foreach(var word in words)
+//	{
 
-		//	}
-		//}
+//	}
+//}
 
-		public static bool IsAnyWordUpperCase_Linq(IEnumerable<string> words)
-		{
-			return words.Any(word => word.All(letter => char.IsUpper(letter)));
+//		public static bool IsAnyWordUpperCase_Linq(IEnumerable<string> words)
+//		{
+//			return words.Any(word => word.All(letter => char.IsUpper(letter)));
 
-		}
+//		}
 
-			public static bool IsAnyWordUpperCase(IEnumerable<string> words)
-		{
-			foreach (var word in words)
-			{
-				bool areAllUpperCase = true;
-				foreach (var letter in word)
-				{
-					if (char.IsLower(letter))
-					{
-						areAllUpperCase = false;
-					}
-				}
+//			public static bool IsAnyWordUpperCase(IEnumerable<string> words)
+//		{
+//			foreach (var word in words)
+//			{
+//				bool areAllUpperCase = true;
+//				foreach (var letter in word)
+//				{
+//					if (char.IsLower(letter))
+//					{
+//						areAllUpperCase = false;
+//					}
+//				}
 
-				if (areAllUpperCase)
-				{
-					return true;
-				}
-			}
+//				if (areAllUpperCase)
+//				{
+//					return true;
+//				}
+//			}
 
-			return false;
-		}
-	}
-}
+//			return false;
+//		}
+//	}
+//}
+
+using System.Net;
+
+var words = new List<string> { "a", "bb", "ccc", "dddd" };
+var wordsLongerThan2Letters = words.Where(word => word.Length > 2);
+
+var numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+var oddNumbers = numbers.Where(number => number % 2 == 1);
+
+Console.ReadKey();
